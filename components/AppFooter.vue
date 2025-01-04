d793f65ab23137d0782e8fa5e8b32e5fd4e5f149
Ivan Sotelo <dev_ivansotelo@capitalcheckin.app>
2025-01-04 06:43:51 +0000

    feat: update footer logo to Fundación 2025 and add new SVG file
    - Replaced the anniversary logo in AppFooter.vue with the new Fundación 2025 logo.
    - Added fundacion_logo.svg to the public directory.

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -4,7 +4,7 @@
       <div class="vertical-align">
         <div class="vertical-align-item">
           <div class="device-content">
-            <nuxt-img src="/Aniversario.svg" alt="115 aniversario" />
+            <nuxt-img src="/Fundacion.svg" alt="Fundación 2025" />
             <nuxt-link class="footer__archive" :to="localePath('history')">
               {{ $t('footer.history') }}
             </nuxt-link>
